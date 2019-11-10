58af8ad9b01be86cd08960c1b6b29bb84f186fba
Stephen Plummer <stephen@Stephens-MBP.frontierlocal.net>
2019-11-09 21:42:42 -0500

    first commit

---
+++ lib/best_alltime.rb
@@ -0,0 +1,6 @@
+require "best_alltime/version"
+
+module BestAlltime
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/best_alltime/version.rb
@@ -0,0 +1,3 @@
+module BestAlltime
+  VERSION = "0.1.0"
+end
